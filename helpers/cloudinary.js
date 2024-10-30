e3951558c100cfbc19b5ec578080211abd7d2d5c
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2024-10-31 01:38:22 +0200

    delete excessive veriable

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -2,10 +2,10 @@ const cloudinary = require("cloudinary").v2;
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
-  process.env;
+const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env;
 
-const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
+const cloud_name = process.env.CLOUDINARY_CLOUD_NAME; //doesn`t work as destructusised
+
 cloudinary.config({
   cloud_name,
   api_key: CLOUDINARY_API_KEY,
